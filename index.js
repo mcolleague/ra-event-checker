c503da19ed5f53a9a74def7ffbf9487c6b024d6a
Max Woodman <max.woodman@huguenots.co.uk>
2021-11-11 22:58:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const chromium = require('chrome-aws-lambda');
+const userAgent = require('user-agents');
+
+const AWS = require('aws-sdk');
+const { PRIORITY_ABOVE_NORMAL } = require('constants');
+const SES = new AWS.SES();
+
+exports.handler = async (event, context, callback) => {    
+    let result = null;
+    let browser = null;
+
+    try {
+        browser = await chromium.puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true
+        });
+
+        let page = await browser.newPage();
+        let title = await page.title();
+
+        const emailParams = {
+            Destination: {
+                ToAddresses: ['maxwoodman@hotmail.co.uk'],
+            },
+            Message: {
+                Body: {
+                    Html: { Data: `Tickets available for event: ${title} (${event.url})` }                    
+                },
+                Subject: {
+                    Data: 'RA checker: tickets available'
+                },
+            },
+            Source: 'max@lambda-interactive.com'
+        }
+
+        await page.setUserAgent(userAgent.toString());
+        await page.goto(event.url || 'https://example.com');
+        await page.waitForSelector('iframe');
+
+        const frameHandle = await page.$('#tickets iframe');
+        const frame = await frameHandle.contentFrame();
+
+        page.on('console', (msg) => console.log(msg.text()));
+
+        result = await frame.evaluate(() => {
+            const openTiers = document.querySelectorAll('[data-ticket-info-selector-id="tickets-info"] li.onsale');
+            const available = openTiers.length > 0;
+            return available;
+        })
+
+        if (result) {
+            console.log('sending email...');
+            try {
+                await SES.sendEmail(emailParams).promise();
+                console.log('email sent successfully');
+            } catch (e) {
+                console.error(e);
+            }            
+        }
+
+        return result;   
+
+    } catch (error) {
+        console.error(error);
+        return callback(error);
+
+    } finally {
+        if (browser !== null) {
+            await browser.close();
+        }
+    }
+
+    return callback(null, result);
+};
